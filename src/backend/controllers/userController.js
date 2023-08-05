69d561886032b6fc645c8e56724b458ab445d729
SheaDuffy13 <13420@coderacademy.edu.au>
2023-08-05 23:07:14 +1000

    feat: user signup login

--- src/backend/controllers/userController.js
+++ src/backend/controllers/userController.js
@@ -15,6 +15,7 @@ exports.signup = async (req, res) => {
 
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 12);
+    console.log(hashedPassword);
 
     // Create new user
     const user = await User.create({
@@ -39,31 +40,33 @@ exports.signup = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
     // Check if user exists
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ error: 'No user with this email' });
     }
 
     // Check password
-    const isMatch = await bcrypt.compare(password, existingUser.password);
-    if (!isMatch) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
+    existingUser.comparePassword(password, (err, isMatch) => {
+      // console.log(isMatch);
+      if (err || !isMatch) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
 
-    // Generate token
-    const token = jwt.sign(
-      { userId: existingUser._id },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' }
-    );
+      // Generate token
+      const token = jwt.sign(
+        { userId: existingUser._id },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      );
 
-    res.status(200).json({ token, userId: existingUser._id });
+      res.status(200).json({ token, userId: existingUser._id });
+    });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
+
 
 // Delete a user by ID
 exports.deleteProfile = async (req, res) => {
--- src/backend/routes/planetRoutes.js
+++ src/backend/routes/planetRoutes.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const planetRouter = express.Router();
-const cloudinary = require("../utils/cloudinary");
-// const { upload } = require('../utils/multer');
-const { getAllPlanets, getPlanet, createPlanet, updatePlanet, deletePlanet } = require('../controllers/planetController')
-const { getImagesForPlanet, getAllImages, addImagesToPlanet, removeImageFromPlanet } = require('../controllers/imageController')
+const { verifyToken } = require('../utils/auth');
+const { getAllPlanets, getPlanet, createPlanet, updatePlanet, deletePlanet } = require('../controllers/planetController');
+const { getImagesForPlanet, getAllImages, addImagesToPlanet, removeImageFromPlanet } = require('../controllers/imageController');
 
-planetRouter.get('/', getAllPlanets);
-// planetRouter.get('/images', getAllImages);
-planetRouter.get('/:id', getPlanet);
-planetRouter.post('/', createPlanet);
-planetRouter.put('/:id', updatePlanet);
-planetRouter.delete('/:id', deletePlanet);
+planetRouter.get('/', verifyToken, getAllPlanets);
+planetRouter.get('/:id', verifyToken, getPlanet);
+planetRouter.post('/', verifyToken, createPlanet);
+planetRouter.put('/:id', verifyToken, updatePlanet);
+planetRouter.delete('/:id', verifyToken, deletePlanet);
 
 //---Image Routes-----------------------------------------------------------------------
-planetRouter.get('/:id/images', getImagesForPlanet);
-// planetRouter.post('/:id/images', addImageToPlanet);
-planetRouter.post('/:id/images', addImagesToPlanet);
-planetRouter.delete('/:id/images/:imageId', removeImageFromPlanet);  
+planetRouter.get('/:id/images', verifyToken, getImagesForPlanet);
+planetRouter.post('/:id/images', verifyToken, addImagesToPlanet);
+planetRouter.delete('/:id/images/:imageId', verifyToken, removeImageFromPlanet);
 
 module.exports = planetRouter;
+
+//-----ORIGINAL--------------------
+// const express = require('express');
+// const planetRouter = express.Router();
+// const cloudinary = require("../utils/cloudinary");
+// // const { upload } = require('../utils/multer');
+// const { getAllPlanets, getPlanet, createPlanet, updatePlanet, deletePlanet } = require('../controllers/planetController')
+// const { getImagesForPlanet, getAllImages, addImagesToPlanet, removeImageFromPlanet } = require('../controllers/imageController')
+
+// planetRouter.get('/', getAllPlanets);
+// // planetRouter.get('/images', getAllImages);
+// planetRouter.get('/:id', getPlanet);
+// planetRouter.post('/', createPlanet);
+// planetRouter.put('/:id', updatePlanet);
+// planetRouter.delete('/:id', deletePlanet);
+
+// //---Image Routes-----------------------------------------------------------------------
+// planetRouter.get('/:id/images', getImagesForPlanet);
+// // planetRouter.post('/:id/images', addImageToPlanet);
+// planetRouter.post('/:id/images', addImagesToPlanet);
+// planetRouter.delete('/:id/images/:imageId', removeImageFromPlanet);  
+
+// module.exports = planetRouter;
