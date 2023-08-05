e026833bd0af7955e4ed6ca936527257c83217cd
SheaDuffy13 <13420@coderacademy.edu.au>
2023-08-06 03:17:41 +1000

    feat: user error handling

--- src/backend/routes/planetRoutes.js
+++ src/backend/routes/planetRoutes.js
@@ -16,26 +16,3 @@ planetRouter.post('/:id/images', verifyToken, addImagesToPlanet);
 planetRouter.delete('/:id/images/:imageId', verifyToken, removeImageFromPlanet);
 
 module.exports = planetRouter;
-
-//-----ORIGINAL--------------------
-// const express = require('express');
-// const planetRouter = express.Router();
-// const cloudinary = require("../utils/cloudinary");
-// // const { upload } = require('../utils/multer');
-// const { getAllPlanets, getPlanet, createPlanet, updatePlanet, deletePlanet } = require('../controllers/planetController')
-// const { getImagesForPlanet, getAllImages, addImagesToPlanet, removeImageFromPlanet } = require('../controllers/imageController')
-
-// planetRouter.get('/', getAllPlanets);
-// // planetRouter.get('/images', getAllImages);
-// planetRouter.get('/:id', getPlanet);
-// planetRouter.post('/', createPlanet);
-// planetRouter.put('/:id', updatePlanet);
-// planetRouter.delete('/:id', deletePlanet);
-
-// //---Image Routes-----------------------------------------------------------------------
-// planetRouter.get('/:id/images', getImagesForPlanet);
-// // planetRouter.post('/:id/images', addImageToPlanet);
-// planetRouter.post('/:id/images', addImagesToPlanet);
-// planetRouter.delete('/:id/images/:imageId', removeImageFromPlanet);  
-
-// module.exports = planetRouter;
--- src/backend/routes/userRoutes.js
+++ src/backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userRouter = express.Router();
+const { verifyToken } = require('../utils/auth');
 const { signup, login, deleteProfile } = require('../controllers/userController')
 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
-userRouter.delete('/deleteProfile', deleteProfile);
+userRouter.delete('/deleteProfile', verifyToken, deleteProfile);
 
 module.exports = userRouter;
